9a1fed0478dbd1a544e40b9bea901ad20c61d0fe
root <root@localhost.localdomain>
2017-05-09 11:26:37 +0700

    v 1.2

--- SimpleWebRequester/simplereqs.py
+++ SimpleWebRequester/simplereqs.py
@@ -6,8 +6,16 @@ import sys
 #r = requests.get(url)
 #print url+' size : '+str(len(r.content))
 
+green = '\033[1;32m'
+red = '\033[1;31m'
+end = '\033[0m'
+ 
 urls = sys.argv[1]
 with open(urls,'rb') as f:
 	for url in f:
 		r = requests.get(url[:-1])
-		print url.strip()+' size '+str(len(r.content))
+		size = len(r.content)
+		if size > 0 :
+			print green+'[*] '+end+url.strip()+' '+green+'size '+str(size)+end
+		else :
+			print red+'[*] '+end+url.strip()+' '+red+'size '+str(size)+end
